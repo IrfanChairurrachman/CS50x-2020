bbc367af590161947c867b9d8cdecc6af6c2a05d
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2020-10-17 20:15:51 +0700

    add: finish speller

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,7 +1,10 @@
 // Implements a dictionary's functionality
-
+#include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -13,15 +16,32 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 65536;
 
 // Hash table
 node *table[N];
+
+// declare variable to count word
+int count = 0;
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
     // TODO
+    int index = hash(word);
+
+    node *cursor = table[index];
+
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+
+        cursor = cursor->next;
+    }
+
     return false;
 }
 
@@ -30,15 +50,16 @@ unsigned int hash(const char *word)
 {
     // TODO
     uint32_t hash = 0;
-    while (*s)
+    while (*word)
     {
-        hash = (hash << 2) ^ (*s | 0x20);
-        s++;
+        hash = (hash << 2) ^ (*word | 0x20);
+        word++;
     }
 
     // return a value between 0 and 65535
     return (int)((hash >> 16) ^ (hash & 0xffff));
 
+    // hash should only be used with N = 65536
     // from curiouskiwi at CS50 Discord
     // https://discord.com/channels/393846237255696385/395685061678071808/766046453750956052
 }
@@ -47,19 +68,78 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    // Open the dictionary file
+    FILE *file = fopen(dictionary, "r");
+
+    if (!file)
+    {
+        printf("Can not open file\n");
+        return false;
+    }
+
+    // declare buffer
+    char word[LENGTH + 1];
+
+    // scan each word
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // create node
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            return false;
+        }
+
+        // copy word to node
+        strcpy(n->word, word);
+        n->next = NULL;
+
+        int index = hash(n->word);
+
+        if (table[index] == NULL)
+        {
+            table[index] = n;
+            count++;
+        }
+        else
+        {
+            n->next = table[index];
+            table[index] = n;
+            count++;
+        }
+    }
+
+    fclose(file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return count;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
     // TODO
-    return false;
+    // iterate each array
+    for (int i = 0; i < N; i++)
+    {
+        node *head = table[i];
+        node *cursor = table[i];
+        node *tmp = table[i];
+
+        // iterate each linkedlist
+        while (cursor != NULL)
+        {
+            cursor = cursor->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    return true;
 }
