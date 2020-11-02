4acdfd25b3ee8c647c59f924fb1cd28a43b24476
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2020-11-02 09:28:14 +0700

    feat: count str (unsolved)

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,7 +1,6 @@
 import sys, csv
 
-
-DB = "databases/small.csv"
+DB = "databases/large.csv"
 
 if len(sys.argv) != 2 and len(sys.argv) != 3:
     sys.exit("Usage: python dna.py [databases] [sequences]")
@@ -16,18 +15,11 @@ with open(database, newline='') as csvfile:
         strs = []
         for dic in m[1:]:
             strs.append(dic)
-            # print(dic)
-        break
-print(strs)
+
 strdict = dict.fromkeys(strs)
-print(strdict)
-strdict['AGATC'] = []
-strdict['AGATC'].append('moo')
-strdict['AGATC'].append('mbe')
-print(strdict)
+
 text = sys.argv[2] if len(sys.argv) == 3 else sys.argv[1]
 
-# print(sys.argv[1])
 txt_file = open(text, "r")
 
 if not txt_file:
@@ -39,29 +31,20 @@ read_txt = txt_file.read()
 print(type(read_txt))
 print(read_txt)
 
-# if read_txt.find("AGATC"):
-#     print("TRUE")
-# i = read_txt.find("AGATC")
-# print(read_txt.find("AGATC"))
 def findstr(txt, strs):
     i = txt.find(strs) + len(strs)
     if txt.find(strs) == 0:
         return 1 + findstr(txt[i:], strs)
     else:
         return 0
-    # return txt
-# print(read_txt[56:].find("AGATC"))
-m = 'AGATC'
-print(read_txt.find(m))
-print(findstr(read_txt[55:], m))
+    
 for key in strs:
-    # m = 0
+    m = read_txt.find(key)
     strdict[key] = []
-    print(key)
-#     while True:
-#         m = read_txt[m:].find(key)
-#         if m == -1:
-#             break
-#         y = findstr(read_txt[m:], key)
-#         strdict[key].append(y)
+    while True:
+        if m == -1:
+            break
+        y = findstr(read_txt[m:], key)
+        strdict[key].append(y)
+        m = read_txt[m + len(key)].find(key)
 print(strdict)
