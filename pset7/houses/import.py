70c0d910ad3e27f3e22ffe892e5c87bbb218628e
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2020-11-11 07:52:16 +0700

    feat: add solved houses

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,26 @@
 # TODO
+import sys, csv, cs50
+
+# connect to database
+db = cs50.SQL("sqlite:///students.db")
+
+# check argv
+if len(sys.argv) != 2:
+    sys.exit("Usage: python import.py [characters.csv]")
+    sys.exit(1)
+
+# open csv
+with open(sys.argv[1], newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # iterate each row in csv
+    for row in reader:
+        # split the name 
+        name = row['name'].split()
+        # if there's no middle name, then insert None in index 1
+        if len(name) == 2:
+            name.insert(1, None)
+        # execute SQL to insert into students.db
+        db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
+                    name[0], name[1], name[2], row['house'], row['birth'])
+        
+        
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,1 +1,18 @@
 # TODO
+import sys, cs50
+
+# check argv
+if len(sys.argv) != 2 or sys.argv[1] not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
+    sys.exit("Usage: python roster.py [house name] or there's no house name")
+    sys.exit(1)
+
+# connect to db
+db = cs50.SQL("sqlite:///students.db")
+# execute SQL command and store to students list
+students = db.execute("SELECT * FROM students WHERE house = (?) order by last, first", sys.argv[1])
+
+for student in students:
+    if student['middle'] == None:
+        print("{} {}, born {}".format(student['first'], student['last'], student['birth']))
+    else:
+        print("{} {} {}, born {}".format(student['first'], student['middle'], student['last'], student['birth']))
